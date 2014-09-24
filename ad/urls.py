1d6548223921fcde567fca2112405f85de3992fb
Simon Panay <simon.panay@free.fr>
2014-09-24 14:00:12 +0200

    add detail and list view for lbc ads

--- ad/urls.py
+++ ad/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, url
 
-from ad.views import prout
+from ad.views import ads_list
+from ad.views import ad_detail
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'flatsearch.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', prout, name='prout'),
+    url(r'^$', ads_list, name='ad-list'),
+    url(r'^(?P<pk>\d+)/$', ad_detail, name='ad-detail'),
 )
--- ad/views.py
+++ ad/views.py
@@ -1,11 +1,16 @@
 import requests
 
-from django.shortcuts import render, redirect
+from django.shortcuts import render_to_response, redirect
+from django.template import RequestContext
 
 from lxml import html
 
+
+LE_BON_COIN_URL = "http://www.leboncoin.fr"
+
+
 # Create your views here.
-def prout(request):
+def ads_list(request):
     payload = {
         'f': 'a',
         'th': '1',
@@ -20,8 +25,23 @@ def prout(request):
         'furn': '2',
         'location': '38000',
     }
-    page = requests.get("http://www.leboncoin.fr/locations/offres/rhone_alpes/isere/", params=payload)
+    page = requests.get(LE_BON_COIN_URL + "/locations/offres/rhone_alpes/isere/", params=payload)
     tree = html.fromstring(page.text)
-    ads_list = tree.xpath('//a/@href')
-    ads = [ad for ad in ads_list if 'location' in ad]
-    print(ads)
+    ads_list = tree.xpath('//div[@class="list-lbc"]//a/@href')
+    ads = [ad.split('locations/')[1].split('.htm?')[0] for ad in ads_list]
+    return render_to_response('ad/ad_list.html',
+                              {'ads_list': ads},
+                              context_instance=RequestContext(request))
+
+
+
+def ad_detail(request, pk):
+    page = requests.get(LE_BON_COIN_URL + "/locations/" + str(pk) + ".htm")
+    tree = html.fromstring(page.text)
+    price = tree.xpath('//span[@class="price"]/text()')
+    thumbs = tree.xpath('//div[@id="thumbs_carousel"]//span/@style')
+    images = [thumb.split("url('")[1].split("');")[0].replace('thumbs', 'images') for thumb in thumbs]
+    return render_to_response('ad/ad_detail.html',
+                              {'price': price,
+                               'images': images,},
+                              context_instance=RequestContext(request))
