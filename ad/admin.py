16281a73d88ec1f9aec07347277928229d3b8646
Simon Panay <simon.panay@free.fr>
2014-09-28 21:15:52 +0200

    add some filter to the admin interface

--- ad/admin.py
+++ ad/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 
 from .models import FlatAd
 
-admin.site.register(FlatAd)
+class FlatAdAdmin(admin.ModelAdmin):
+    list_filter = (
+        'ges',
+        'energy_class',
+        'rooms',
+        'charges_included',
+        'city',
+    )
+
+admin.site.register(FlatAd, FlatAdAdmin)
